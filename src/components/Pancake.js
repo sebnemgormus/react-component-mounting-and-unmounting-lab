6cb40394e130b0abfa9ebacad6386f7ec435a40c
Sebnem <sebnemgormus@gmail.com>
2022-03-10 19:38:03 +0300

    commit tree f4a2cef62d868c578845144cfb091b88b42d48d6

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -10,9 +10,17 @@ class Pancake extends React.Component {
     };
   }
 
-  // TODO: create a componentDidMount() which will start the interval to count how long the pancake has been cooking
+  // componentDidMount() which will start the interval to count how long the pancake has been cooking
 
-  // TODO: create a componentWillUnmount() which will clear the interval
+  // componentWillUnmount() which will clear the interval
+
+  componentDidMount() {
+    this.startInterval();
+  }
+
+  componentWillUnmount() {
+    this.cleanUpInterval()
+  }
 
   updateCounter = () => {
     this.setState({
